1f68dd1d2f65b2fa2701f4f609a2498b6a6d8315
Kanishka Garg <kanishka080199@gmail.com>
2019-03-15 09:24:27 +0530

    Initial Commit

---
+++ basic.py
@@ -0,0 +1,44 @@
+from sys import *
+
+tokens = []
+
+def open_file(filename):
+	data = open(filename,'r').read()
+	return data
+
+def lex(filecontents):
+	tok = ""
+	state = 0
+	string = ""
+	filecontents = list(filecontents)
+	for char in filecontents:
+		tok += char
+		if tok == " ":
+			if state == 0:
+				tok = ""
+			else:
+				tok = " "
+		elif tok == "PRINT":
+			tokens.append("PRINT")
+			tok = ""
+		elif tok == "\"":
+			if state == 0:
+				state = 1
+			elif state == 1:
+				tokens.append("STRING:" + string + "\"")
+				string = ""
+				state = 0
+		elif state == 1:
+			string += tok
+			tok = ""
+	#print(tokens)
+
+def parse(toks):
+	print(toks)
+
+def run():
+	data = open_file(argv[1])
+	toks = lex(data)
+	parse(toks)
+
+run()
